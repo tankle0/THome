1788a71432b453136f016eda84df9b4bfe58b8e2
陈潭 <itw_chentan@tkmail.com>
2021-12-30 10:58:16 +0800

    feat: add page link

--- src/views/home/Header.vue
+++ src/views/home/Header.vue
@@ -54,7 +54,9 @@ const router = useRouter()
         </span>
         <template #dropdown>
           <el-dropdown-menu>
-            <el-dropdown-item >首页</el-dropdown-item>
+            <router-link to="/">
+              <el-dropdown-item >首页</el-dropdown-item>
+            </router-link>
             <el-dropdown-item divided>个人中心</el-dropdown-item>
             <el-dropdown-item @click="loginOut" divided>退出登录</el-dropdown-item>
           </el-dropdown-menu>

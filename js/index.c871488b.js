67bd91feefd2eb9f6c1d35a07963c8189ab3df87
tankle0 <63094764+tankle0@users.noreply.github.com>
2022-02-20 08:52:06 +0000

    Deploying to gh-pages from @ tankle0/THome@9d792fd5ddc0a8937f6cff22cc5a6443035a80a9 🚀

---
+++ js/index.c871488b.js
@@ -0,0 +1,1 @@
+import{_ as a}from"./index.370be7ca.js";import{o,f as s,P as t,Q as _,g as d}from"./vendor.6667a01e.js";const c={name:"Dashboard"},r=e=>(t("data-v-4f2d5abf"),e=e(),_(),e),n={class:"dashboard"},p=r(()=>d("h1",null,"tank-\u9996\u9875",-1)),i=[p];function f(e,u,h,l,m,v){return o(),s("div",n,i)}var I=a(c,[["render",f],["__scopeId","data-v-4f2d5abf"]]);export{I as default};

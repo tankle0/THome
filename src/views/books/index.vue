1e40ab4f8a18debd50bad83104baf743556fc250
tank <63094764+tankle0@users.noreply.github.com>
2026-05-16 09:22:46 +0800

    Add iframe for external content in books view
    测试

--- src/views/books/index.vue
+++ src/views/books/index.vue
@@ -11,6 +11,7 @@ export default{
 <template>
   <div class="books">
     <h1>tank-学习资料</h1>
+    <iframe src="https://wlhy.muyuan56.com" frameborder="0"></iframe>
   </div>
 </template>
 
@@ -19,5 +20,9 @@ export default{
   width: 100%;
   height: 100%;
   background-color: #fff;
+  iframe{
+    width: 100%;
+    min-height: 70vh;
+  }
 }
 </style>
